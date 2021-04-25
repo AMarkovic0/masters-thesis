e7f5dc2174927cab9f3dfad61ba7eb229fffbac5
Aleksa Markovic <malexa96@gmail.com>
2021-04-25 18:51:05 +0200

    broker modified regarding driver api requirements. ID is socket id not thread ID

--- inc/broker.h
+++ inc/broker.h
@@ -5,16 +5,18 @@
 #include<malloc.h>
 #include<stdint.h>
 #include<string.h>
+#include<pthread.h>
 
 #define BUF_SIZE 255
+#define NAME_SIZE 32
 
 typedef enum driver_states{ BORN , HANDSHAKE , INIT , READY , CLOSED }drivstat_t;
 
 typedef struct drivers_list
 {
-	int thread_id;
-	char* name;
-	char* msg;
+	int driver_id;
+	char name[ NAME_SIZE ];
+	char msg[ BUF_SIZE ];
 	struct drivers_list* next;
 }_drivList;
 
@@ -32,12 +34,23 @@ typedef struct broker_queue
         unsigned int size;
 }_brokerQ;
 
+int broker_setup( void );
+
 void brokerQ_push( int from , int to , char msg[ ] );
 int brokerQ_pop( char* msg );
 uint8_t brokerQ_isempty( void );
 
+void drivList_add( int driver_id , char name[ ] );
+pthread_t drivList_del( char name[ ] );
+_drivList drivList_find( char name[ ] );
+
+extern pthread_mutex_t lockQ;
+extern pthread_mutex_t lockList;
+
 extern _brokerQ broker;
 extern _drivList driver_list;
+
+extern _drivList* drivers_list;
 
 #endif //BROKER
 
--- src/broker.c
+++ src/broker.c
@@ -1,10 +1,34 @@
 #include "broker.h"
 
+pthread_mutex_t lockQ;
+pthread_mutex_t lockList;
+
 _brokerQ broker;
 _drivList driver_list;
 
+_drivList* drivers_list;
+
+int broker_setup( void )
+{
+	if ( 0 != pthread_mutex_init( &lockQ , NULL ) )
+	{
+		printf( "\n Mutex init failed\n" );
+		return -1;
+	}
+
+	if ( 0 != pthread_mutex_init( &lockList , NULL ) )
+	{
+		printf( "\n Mutex init failed\n" );
+		return -1;
+	}
+
+	return 1;
+}
+
 void brokerQ_push( int from , int to , char msg[ ] )
 {
+	pthread_mutex_lock( &lockQ );
+
 	if( NULL == broker.head )
 	{
 		broker.head = ( _drivMsg* )calloc( 1 , sizeof( _drivMsg ) );
@@ -12,6 +36,9 @@ void brokerQ_push( int from , int to , char msg[ ] )
 		strcpy( broker.head->msg , msg );
 		broker.head->next = NULL;
 		broker.size++;
+
+		pthread_mutex_unlock( &lockQ );
+
 		return;
 	}
 
@@ -25,20 +52,27 @@ void brokerQ_push( int from , int to , char msg[ ] )
 	strcpy( tmp->next->msg , msg );
 	tmp->next->next = NULL;
 	broker.size++;
-	return;
 
+	pthread_mutex_unlock( &lockQ );
+
+	return;
 }
 
 int brokerQ_pop( char* msg )
 {
-	 if( 0 == broker.size )
-                return 0;
+
+	if( 0 == broker.size )
+		return 0;
+
+	pthread_mutex_lock( &lockQ );
 
 	strcpy( msg , broker.head->msg );
 	_drivMsg* next = broker.head->next;
-        free( broker.head );
-        broker.head = next;
-        broker.size--;
+	free( broker.head );
+	broker.head = next;
+	broker.size--;
+
+	pthread_mutex_unlock( &lockQ );
 
 	return (int)strlen( msg );
 }
@@ -48,4 +82,97 @@ uint8_t brokerQ_isempty( void )
 	return ( 0 == broker.size );
 }
 
+void drivList_add( int driver_id , char name[ ] )
+{
+	pthread_mutex_lock( &lockList );
+
+	if( NULL == drivers_list )
+	{
+		_drivList* tmp = (_drivList*)calloc( 1 , sizeof( _drivList ) );
+		tmp->driver_id = driver_id;
+		strcpy( tmp->name , name );
+		tmp->next = drivers_list;
+		drivers_list = tmp;
+
+		pthread_mutex_unlock( &lockList );
+
+		return;
+	}
+
+	_drivList* tmp = drivers_list;
+	while(NULL != tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = (_drivList*)calloc( 1 , sizeof( _drivList ) );
+	tmp->next->driver_id = driver_id;
+	strcpy( tmp->next->name , name );
+
+	pthread_mutex_unlock( &lockList );
+
+	return;
+}
+
+pthread_t drivList_del( char name[ ] )
+{
+	pthread_mutex_lock( &lockList );
+
+	_drivList* _c = drivers_list;
+	_drivList* _p = NULL;
+	int driver_id = 0;
+
+	if( NULL == drivers_list )
+	{
+		pthread_mutex_unlock( &lockList );
+		return 0;
+	}
+
+	while( 0 != strcmp( _c->name , name ) )
+	{
+		if( NULL == _c->next )
+		{
+			pthread_mutex_unlock( &lockList );
+			return 0;
+		}
+		_p = _c;
+		_c = _c->next;
+	}
+
+	if( _c == drivers_list )
+		drivers_list = _c->next;
+	else
+		_p->next = _c->next;
+
+	driver_id = _c->driver_id;
+	pthread_mutex_unlock( &lockList );
+
+	return driver_id;
+
+}
+
+_drivList drivList_find( char name[ ] )
+{
+	pthread_mutex_lock( &lockList );
+
+	_drivList* tmp = drivers_list;
+
+	if( NULL == tmp )
+	{
+		pthread_mutex_unlock( &lockList );
+		return *drivers_list;
+	}
+
+	while( 0 != strcmp( tmp->name , name ) )
+	{
+		if( NULL == tmp->next )
+		{
+			pthread_mutex_unlock( &lockList );
+			return *drivers_list;
+		}
+		tmp = tmp->next;
+	}
+
+	pthread_mutex_unlock( &lockList );
+
+	return *tmp;
+}
 
