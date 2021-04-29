e301923325f4117e263dc704c2fa3baed94769e0
Aleksa Markovic <malexa96@gmail.com>
2021-04-29 22:17:26 +0200

    Fixed getting out bug when broker reads drivers messages.

--- main.c
+++ main.c
@@ -24,13 +24,15 @@ int main( int argc , char* argv[ ] )
 
 	pthread_create( &thread_id[ 0 ] , NULL , &broker_exec , NULL );
 
-	for( int i = 1 ; i < NUM_OF_DEVICES ; i++ )
-	{
-		tcp_server_accept( &newSocket[ i ] , &newAddr[ i ] , LOG_ON );
-		pthread_create( &thread_id[ i ] , NULL , &gen_alg_driv_exec , (void*)&newSocket[ i ] );
-		printf( "THREAD ID : %lu \r\n" , (uint64_t)thread_id[ i ] );
-		sleep( 0.4 );
-	}
+	tcp_server_accept( &newSocket[ 1 ] , &newAddr[ 1 ] , LOG_ON );
+	pthread_create( &thread_id[ 1 ] , NULL , &gen_alg_driv_exec , (void*)&newSocket[ 1 ] );
+
+	sleep( 3 );
+
+	tcp_server_accept( &newSocket[ 2 ] , &newAddr[ 2 ] , LOG_ON );
+	pthread_create( &thread_id[ 2 ] , NULL , &test_driv_exec , (void*)&newSocket[ 2 ] );
+
+	sleep( 3 );
 
 	for( int i = 0 ; i < NUM_OF_DEVICES ; i++ )
 		pthread_join( thread_id[ i ] , NULL );
@@ -55,8 +57,19 @@ void* broker_exec( void* vargp )
 			if( 0 != rmsg[ 0 ] )
 			{
 				printf( "%s %d %d \r\n" , rmsg , from , to );
-				while( ( NULL != list ) && ( list->driver_id != to ) )
-					list = list->next;
+				while( NULL != list )
+				{
+					if( list->driver_id != to )
+					{
+						printf( "Listing: %s \r\n" , list->name );
+						list = list->next;
+					}
+					else
+					{
+						break;
+					}
+				}
+
 				if( NULL != list )
 				{
 					strcat( list->msg , rmsg );
